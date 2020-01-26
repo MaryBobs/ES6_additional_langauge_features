bcb15aebe22911d9ef9227ee30a029cd8631a5fd
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2020-01-26 14:20:58 +0000

    refactors Library  with class syntax

--- hw_es6_features_start/library.js
+++ hw_es6_features_start/library.js
@@ -1,23 +1,50 @@
-const Library = function (books = []) {
-  this.books = books;
-};
+// const Library = function (books = []) {
+//   this.books = books;
+// };
+//
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+//
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+//
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+//
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+//
+// module.exports = Library;
 
-Library.prototype.bookCount = function () {
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  bookCount() {
   return this.books.length;
 };
 
-Library.prototype.addBook = function (newBook) {
+  addBook(newBook) {
   this.books.push(newBook);
 };
 
-Library.prototype.addBooks = function (newBooks) {
+  addBooks(newBooks) {
   newBooks.forEach(book => this.books.push(book));
 };
 
-Library.prototype.printInventory = function () {
+  printInventory() {
   this.books.forEach((book) => {
     console.log(`${book.title} by ${book.author}`);
   });
+};
+
 };
 
 module.exports = Library;
